c534f16ce71551b5af0d90945a81c320ba62e3df
Lucian Villaseñor <lucianv_001@yahoo.com>
2019-10-31 13:20:11 -0500

    Update ssh_server.pp

--- site/role/manifests/ssh_server.pp
+++ site/role/manifests/ssh_server.pp
@@ -1,4 +1,15 @@
-class role::db_server {
-  include profile::base
-  include profile::ssh
-  }
+class profile::ssh_server {
+	package {'openssh-server':
+		ensure => present,
+	}
+	service { 'sshd':
+		ensure => 'running',
+		enable => 'true',
+	}
+	ssh_authorized_key { 'root@master.puppet.vm':
+		ensure => present,
+		user   => 'root',
+		type   => 'ssh-rsa',
+		key    => '',
+	}  
+}
